3c50454c4a45b58c6146aa52d5e92a5a9d4212a4
Illia <144327680+ilyaghrischenko@users.noreply.github.com>
2025-01-07 21:32:50 +0200

    add base mailer host

---
+++ backend/microservices/Jeda.Mailer/Jeda.Mailer/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Hosting;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+         
+    })
+    .Build();
+    
+await host.RunAsync();
