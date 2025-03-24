22e7b033c33734e6c58f8302d04119307a16fbc6
Timofii Solovey <116516370+SoloveyTimofey@users.noreply.github.com>
2025-03-24 12:07:51 +0200

    create page switching logic without any animations
    I'd like to add animations after discussing it with the rest of the team

---
+++ pages/components/why-choose-us.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const WhyChooseUs: FC = () => {
+    return (
+        <div>
+            <h1 style={{ color: "#fff" }}>Why Choose Us Working</h1>
+        </div>
+    )
+}
+
+export default WhyChooseUs;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,86 @@
-import { FC } from "react";
+import { Dispatch, FC, RefObject, SetStateAction, useEffect, useRef, useState } from "react";
 import sass from '/styles/mainpage.module.scss'
 import MainComponent from "./components/main-component";
+import WhyChooseUs from "./components/why-choose-us";
+import StackTechnologies from "./components/stack-techtologies";
 
 const Home: FC = () => {
+  const mainRef = useRef<HTMLDivElement | null>(null);
+  const whyChooseUsRef = useRef<HTMLDivElement | null>(null);
+  const stackTechnologiesRef = useRef<HTMLDivElement | null>(null);
+
+  const [currentPage, setCurrentPage] = useState(CurrentPage.Main)
+
+  useEffect(() => {
+    let throttleTimeout: NodeJS.Timeout | null = null; // Use timeout to prevent too often method calls, for example after strong scroll
+
+    throttleTimeout = setTimeout(() => {
+      throttleTimeout = null;
+    }, 300)
+
+    const handleWheel = (event: WheelEvent) => {
+      if(throttleTimeout) return;
+
+      event.preventDefault();
+
+      if(Math.abs(event.deltaY) > 40){
+        switch (currentPage) {
+          case CurrentPage.Main:
+            if(event.deltaY > 0){
+              scrollIntoSpecifiedView(setCurrentPage, CurrentPage.WhyChooseUs, whyChooseUsRef);
+            }
+            break;
+          case CurrentPage.WhyChooseUs:
+            if(event.deltaY > 0){
+              scrollIntoSpecifiedView(setCurrentPage, CurrentPage.StackTechnologies, stackTechnologiesRef);
+            } else {
+              scrollIntoSpecifiedView(setCurrentPage, CurrentPage.Main, mainRef);
+            }
+            break;
+          case CurrentPage.StackTechnologies:
+            if(event.deltaY < 0){
+              scrollIntoSpecifiedView(setCurrentPage, CurrentPage.WhyChooseUs, whyChooseUsRef);
+            }
+            break;
+          default:
+            console.log("NONE") 
+            break;
+        }
+      }
+    };
+
+    window.addEventListener('wheel', handleWheel, { passive: false });
+
+    return () => window.removeEventListener('wheel', handleWheel);
+  }, [currentPage]);
+
   return (
     <div className={sass.layout}>
-        <div className={sass.content_container}>
+      <div ref={mainRef} id="main" className={sass.content_container}>
         <MainComponent />
-        </div>
+      </div>
+      <div ref={whyChooseUsRef} id="whyChooseUs" className={sass.content_container}>
+        <WhyChooseUs />
+      </div>
+      <div ref={stackTechnologiesRef} id="stackTechnologies" className={sass.content_container}>
+        <StackTechnologies />
+      </div>
     </div>
   );
 };
+
+function scrollIntoSpecifiedView(setCurrentPage: Dispatch<SetStateAction<CurrentPage>>, nextPage: CurrentPage, ref: RefObject<HTMLDivElement | null>){
+  ref.current?.scrollIntoView({
+    behavior: 'smooth'
+  })
+  setCurrentPage(nextPage);
+}
+
+enum CurrentPage {
+  Main,
+  WhyChooseUs,
+  StackTechnologies
+}
+
 
 export default Home;
